e17adfe7e7cb64b754e6aec343b63f59d604aadf
Shazisaremi <shazi.saremi@torrens.edu.au>
2022-09-18 20:34:34 +0430

    Module 2 Program Update

--- Module_2/Program.cs
+++ Module_2/Program.cs
@@ -6,9 +6,42 @@ namespace Module_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 12 * 30;
+            Console.WriteLine(x);
 
-            Console.WriteLine("Hello World 2!");
+
+            int myNum = 5;               // Integer (whole number)
+            double myDoubleNum = 5.99D;  // Floating point number
+            char myLetter = 'D';         // Character
+            bool myBool = true;          // Boolean
+            string myText = "Hello";     // String
+
+
+            int myNewNum;
+            myNewNum = 15;
+            Console.WriteLine(myNewNum);
+
+
+            int myInt = 9;
+            double myDouble = myInt;       // Automatic casting: int to double
+            Console.WriteLine(myInt);      // Outputs 9
+            Console.WriteLine(myDouble);   // Outputs 9
+
+
+            double myDouble2 = 9.78;
+            int myInt2 = (int)myDouble2;    // Manual casting: double to int
+            Console.WriteLine(myDouble2);   // Outputs 9.78
+            Console.WriteLine(myInt2);      // Outputs 9
+
+
+            int myInt3 = 10;
+            double myDouble3 = 5.25;
+            bool myBool3 = true;
+
+            Console.WriteLine(Convert.ToString(myInt3));    // convert int to string
+            Console.WriteLine(Convert.ToDouble(myInt3));    // convert int to double
+            Console.WriteLine(Convert.ToInt32(myDouble3));  // convert double to int
+            Console.WriteLine(Convert.ToString(myBool3));   // convert bool to string
         }
     }
 }
