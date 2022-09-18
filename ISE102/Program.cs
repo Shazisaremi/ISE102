f0227a3f2db6a7cf3f289ce8f7331a3667962522
R-Goldenhand <rezazarindast76@gmail.com>
2022-09-18 19:35:47 +0430

    Add project files.

---
+++ ISE102/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISE102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Module_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
