3889d56843aa1a678b003b0378c0dc35e54aa6eb
Shazisaremi <shazi.saremi@torrens.edu.au>
2022-09-18 19:37:22 +0430

    First Commit

--- ISE102/Program.cs
+++ ISE102/Program.cs
@@ -6,7 +6,7 @@ namespace ISE102
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Wellcome To ISE102 !");
         }
     }
 }
