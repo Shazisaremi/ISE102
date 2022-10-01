2b0b37130da41545758b26a13da20b313ab8df94
Shazisaremi <shazi.saremi@torrens.edu.au>
2022-10-01 17:09:35 +0330

    Module 3 UPdated regex subject

--- Module_3/Program.cs
+++ Module_3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Module_3;
+using System.Text.RegularExpressions;
 
 namespace Module_3
 {
@@ -7,13 +8,27 @@ namespace Module_3
     {
         static void Main(string[] args)
         {
-            Exercise_1.Code.main();
+            //Exercise_1.Code.main();
 
-            Exercise_2.Code.main();
+            //Exercise_2.Code.main();
 
-            Exercise_3.Code.main();
+            //Exercise_3.Code.main();
 
-            Exercise_4.Code.main();
+            //Exercise_4.Code.main();
+
+            string alpha = @"[a-zA-Z]"; // Alphabetic characters Regex
+            string digit = @"\d"; // Digital Characters Regex
+            string specc = @"\W"; // Special Characters Regex
+
+            string password = "Hello World";
+
+            bool has_alpha = Regex.IsMatch(password, alpha);
+            bool has_digit = Regex.IsMatch(password, digit);
+            bool has_specc = Regex.IsMatch(password, specc);
+
+            Console.WriteLine(has_alpha);
+            Console.WriteLine(has_digit);
+            Console.WriteLine(has_specc);
         }
     }
 }
---
+++ Module_3/README.md
@@ -0,0 +1,29 @@
+﻿## C# Regular Expressions
+
+### Code:
+```csharp
+using System.Text.RegularExpressions;
+
+string alpha = @"[a-zA-Z]"; // Alphabetic characters Regex
+string digit = @"\d"; // Digital Characters Regex
+string specc = @"\W"; // Special Characters Regex
+
+string password = "Hello World";
+
+bool has_alpha = Regex.IsMatch(password, alpha);
+bool has_digit = Regex.IsMatch(password, digit);
+bool has_specc = Regex.IsMatch(password, specc);
+
+Console.WriteLine(has_alpha);
+Console.WriteLine(has_digit);
+Console.WriteLine(has_specc);
+```
+
+### Links : 
+A good site to train regex coding in many languages:
+
+https://regex101.com/
+
+cheat sheet:
+
+https://www.mikesdotnetting.com/article/46/c-regular-expressions-cheat-sheet
