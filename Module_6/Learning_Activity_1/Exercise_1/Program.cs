70be7e5c1de5f87bd03635acd09ce212e39598e3
Shazisaremi <shazi.saremi@torrens.edu.au>
2022-10-20 06:42:39 +0330

    UuPDATE module 6

--- Module_6/Learning_Activity_1/Exercise_1/Program.cs
+++ Module_6/Learning_Activity_1/Exercise_1/Program.cs
@@ -1,19 +1,19 @@
 ﻿public class BankAccount
 {
-    public string Number { get; }
-    public string Owner { get; set; }
-    public decimal Balance { get; }
+    public string Number { get; } // Define number property that just support get
+    public string Owner { get; set; } // Define Owner property that support both get and set
+    public decimal Balance { get; } // Define Balance property that just support get
 
-    public BankAccount(string name, decimal initialBalance)
+    public BankAccount(string name, decimal initialBalance) // Constructor with two inputs : name initial balance
     {
         Owner = name;
         Balance = initialBalance;
     }
-    public void MakeDeposit(decimal amount, DateTime date, string note)
+    public void MakeDeposit(decimal amount, DateTime date, string note) // Make Deposit method
     {
     }
 
-    public void MakeWithdrawal(decimal amount, DateTime date, string note)
+    public void MakeWithdrawal(decimal amount, DateTime date, string note) // Make Withdrawal method. 
     {
     }
 }
